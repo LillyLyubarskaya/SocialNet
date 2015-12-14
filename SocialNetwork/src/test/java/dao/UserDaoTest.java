191197c50a58c7b6341273db120d09023482e1b6
LillyLyubarskaya <lyubarskaya@outlook.com>
2015-12-14 22:34:44 +0400

    Changed tests

--- SocialNetwork/src/test/java/dao/UserDaoTest.java
+++ SocialNetwork/src/test/java/dao/UserDaoTest.java
@@ -1,7 +1,16 @@
 package dao;
 
+import model.User;
+import org.junit.Assert;
+import org.junit.Test;
 /**
  * Created by Lilly_94 on 14.12.2015.
  */
 public class UserDaoTest {
+//    @Test
+//    public void getByIdTest(){
+//        UserDao userDao=new UserDao();
+//        User user=userDao.getById(1);
+//        Assert.assertTrue(user.getEmail().compareTo("haruki@mail.com")==0);
+//    }
 }
