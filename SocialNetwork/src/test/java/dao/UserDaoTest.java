7b3719ff24d1fa6af66d3c50cd67d4b8a0bd588a
LillyLyubarskaya <lyubarskaya@outlook.com>
2015-12-20 19:54:20 +0400

    Fixed test

--- SocialNetwork/src/test/java/dao/UserDaoTest.java
+++ SocialNetwork/src/test/java/dao/UserDaoTest.java
@@ -1,16 +1,43 @@
 package dao;
 
+import logic.UserLogic;
 import model.User;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
-/**
- * Created by Lilly_94 on 14.12.2015.
- */
+import java.util.List;
 public class UserDaoTest {
+    @Ignore
     @Test
-    public void getByIdTest(){
+    public void getAllTest(){
         UserDao userDao=new UserDao();
-        User user=userDao.getById(1);
-        Assert.assertTrue(user.getEmail().compareTo("haruki@mail.com")==0);
+        List<User> res=userDao.getAll();
+        Assert.assertTrue(res.get(0).getEmail().compareTo("haruki@mail.com")==0);
+    }
+    @Ignore
+    @Test
+    public void createTest(){
+        UserLogic userLogic=UserLogic.getInstance();
+        User user=new User();
+        user.setEmail("xxc@mailo.com");
+        user.setPassword("XWEERRaaa");
+        userLogic.createUser(user);
+        User u=userLogic.getUser("xxc@mailo.com","XWEERRaaa");
+        Assert.assertTrue(u!=null);
+    }
+    @Ignore
+    @Test
+    public void deleteTest(){
+        UserLogic userLogic=UserLogic.getInstance();
+        User user=new User();
+        user.setEmail("qwerty@mail.com");
+        user.setPassword("Awe2rGfuz");
+        userLogic.createUser(user);
+        User u=userLogic.getUser("qwerty@mail.com","Awe2rGfuz");
+        Assert.assertTrue(u!=null);
+        UserDao userDao=new UserDao();
+        userDao.delete(user);
+        u=userLogic.getUser("qwerty@mail.com","Awe2rGfuz");
+        Assert.assertTrue(u==null);
     }
 }
